93d327fcfa63f9148de7177f181f579f98ef96f8
Will <willplum01@gmail.com>
2021-03-15 21:23:43 -0700

    Rewrote how the user's input is checked, still not happy with it since I'll need to add more and more if statements if I want more possible selections, but I'll have to figure out how to do that more efficiently later. For now what I have seems to work. I've also found out that there doesn't seem to be any good method of clearing the previous output, so it'll look at least a bit messy. That's ok, just not prefered. Next I plan on continuing to work on cleaning commandMenu.cpp and starting on the program which will govern the sructure of the turn based battles. I really should've started on that first, but I'll live and learn.

--- commandMenu.cpp
+++ commandMenu.cpp
@@ -4,42 +4,49 @@ Secondly, the user is prompted to type the command they wish to execute.
 Then, the program should check the user's responce and take the appropriate action.*/
 
 #include <iostream>
+#include <string>
+#include <algorithm>
 
-//Display command meny "graphics"
+//Display command menu "graphics"
 void menu(std::string x) {
 
-   if (x == "Menu") {
-   std::cout << "0--------------------0\n|______COMMANDS______|\n| " << ">" << "Attack            |\n| " << ">" << "Spells            |\n| " << ">" << "Defend            |\n0--------------------0" << std::endl;
-   }
-
-   if (x == "Spell") {
-   std::cout << "0--------------------0\n|______COMMANDS______|\n| >Flare-1           |\n| >Lightning-2       |\n0--------------------0" << std::endl;
+   if (x == "main") {
+      std::cout << "0--------------------0\n|______COMMANDS______|\n| " << ">" << "Attack            |\n| " << ">" << "Spells            |\n| " << ">" << "Defend            |\n0--------------------0" << std::endl;
+   } else if (x == "SPELLS" || x == "SPELL") {
+      std::cout << "0--------------------0\n|______COMMANDS______|\n| >Flare-1           |\n| >Lightning-2       |\n0--------------------0" << std::endl;
    }
 }
 
-//Choose action to be taken based off of user input
-int selection(std::string x) {
+void spellManage() {
+   std::string z;
+   std::cout << "What spell?\n";
+   std::cin >> z;
+   std::cout << z << " was selected" << std::endl;
+}
 
-   return 0;
+
+//Choose action to be taken based off of user input
+void optManage(std::string x) {
+   if (x == "SPELLS" || x == "SPELL") {
+      menu(x);
+      spellManage();
+   } else if (x == "ATTACK") {
+      std::cout << "Selected the attack command\n";
+   } else if (x== "DEFEND") {
+      std::cout << "Selected the defence command\n";
+   }
 }
 
 
 int main() {
 
-   std::string options[] = {"Attack", "Spell", "Defend"};
-
-   std::string choice = "Menu";
-
+   std::string choice = "main";
    menu(choice);
 
-   std::cout << "Chose your option: " << std::endl;
+   std::cout << "Enter your choice\n";
    std::cin >> choice;
-
-   for (int x = 0; x < sizeof(options)/sizeof(options[0]); x = x + 1) {
-   if (choice == options[x]) {
-      menu(choice);
-      }
-   }
+   transform(choice.begin(), choice.end(), choice.begin(), ::toupper);
+   optManage(choice);
 
    return 0;
 }
